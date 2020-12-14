df2974ad182a860e88ab5c6f311501e5f30ae396
Sebastian Michel <sebastian.michel96@gmail.com>
2020-12-14 09:33:47 +0100

    Fixed #16 (#20)
    (cherry picked from commit cf3ab1b7140a6d8ca7b40dbd604adb9f4948b454)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tod0",
-    version="0.6.0",
+    version="0.6.1",
     author="kiblee",
     author_email="kiblee@pm.me",
     packages=find_packages(),
