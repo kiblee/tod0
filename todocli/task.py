b473644a346aab118f7e506435d147e8e40dba65
Sebastian Michel <sebastian.michel96@gmail.com>
2020-12-21 10:32:39 +0100

    Fixed rebase problem

--- todocli/task.py
+++ todocli/task.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from enum import Enum
 
-from todocli.todo_api_util import api_timestamp_to_datetime
+from todocli.todo_api.todo_api_util import api_timestamp_to_datetime
 
 
 class Task:
