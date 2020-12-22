c6d29e488877e128c722233d9e6fba3fbc55a072
Sebastian Michel <sebastian.michel96@gmail.com>
2020-12-22 18:23:49 +0100

    Renamed query_result_task to query_result, removed the explicit type specifiers

--- todocli/task.py
+++ todocli/task.py
@@ -17,40 +17,40 @@ class Task:
         Normal = "normal"
         High = "high"
 
-    def __init__(self, query_result_task):
-        self.title: str = query_result_task["title"]
-        self.id: str = query_result_task["id"]
-        self.importance: Task.Importance = Task.Importance(
-            query_result_task["importance"]
+    def __init__(self, query_result):
+        self.title = query_result["title"]
+        self.id = query_result["id"]
+        self.importance = Task.Importance(
+            query_result["importance"]
         )
-        self.status: Task.Status = Task.Status(query_result_task["status"])
-        self.created_datetime: datetime = api_timestamp_to_datetime(
-            query_result_task["createdDateTime"]
+        self.status = Task.Status(query_result["status"])
+        self.created_datetime = api_timestamp_to_datetime(
+            query_result["createdDateTime"]
         )
 
-        if "completedDateTime" in query_result_task:
-            self.completed_datetime: datetime = api_timestamp_to_datetime(
-                query_result_task["completedDateTime"]
+        if "completedDateTime" in query_result:
+            self.completed_datetime = api_timestamp_to_datetime(
+                query_result["completedDateTime"]
             )
         else:
             self.completed_datetime = None
 
-        self.is_reminder_on: bool = bool(query_result_task["isReminderOn"])
+        self.is_reminder_on: bool = bool(query_result["isReminderOn"])
 
-        if "reminderDateTime" in query_result_task:
-            self.reminder_datetime: datetime = api_timestamp_to_datetime(
-                query_result_task["reminderDateTime"]
+        if "reminderDateTime" in query_result:
+            self.reminder_datetime = api_timestamp_to_datetime(
+                query_result["reminderDateTime"]
             )
         else:
             self.reminder_datetime = None
 
-        self.last_modified_datetime: datetime = api_timestamp_to_datetime(
-            query_result_task["lastModifiedDateTime"]
+        self.last_modified_datetime = api_timestamp_to_datetime(
+            query_result["lastModifiedDateTime"]
         )
 
-        if "bodyLastModifiedDateTime" in query_result_task:
-            self.body_last_modified_datetime: datetime = api_timestamp_to_datetime(
-                query_result_task["bodyLastModifiedDateTime"]
+        if "bodyLastModifiedDateTime" in query_result:
+            self.body_last_modified_datetime = api_timestamp_to_datetime(
+                query_result["bodyLastModifiedDateTime"]
             )
         else:
             self.body_last_modified_datetime = None
